8ed0d87388aeb9de47f5e84f45ecec32680b738c
Fisual <rolmine2006@gmail.com>
2022-07-07 18:37:09 +0700

    some fixes

--- particles/particles.cpp
+++ particles/particles.cpp
@@ -1006,7 +1006,7 @@ void CParticleCollection::Init( CParticleSystemDefinition *pDef, float flDelay, 
 	}
 	else
 	{
-		m_nRandomSeed = (intp)this;
+		m_nRandomSeed = (int)(intp)this;
 #ifndef _DEBUG
 		m_nRandomSeed += Plat_MSTime();
 #endif
